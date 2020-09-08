2d7c7f881cc3c7d45103ddb05f154b72a1f19188
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2020-09-08 15:45:19 -0400

    Update version to 2.12.8

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'LiteSpeed Technologies'
 # The short X.Y version
 version = u'2.12'
 # The full version, including alpha/beta/rc tags
-release = u'2.12.7'
+release = u'2.12.8'
 
 
 # -- General configuration ---------------------------------------------------
