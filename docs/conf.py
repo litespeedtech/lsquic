6eec0a76f7edc4e7c6e6d32305484065f2fa7478
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2020-10-06 16:54:03 -0400

    Release 2.12.10
    - [BUGFIX] Deactivate only *recent* HQ frame, not any HQ frame.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'LiteSpeed Technologies'
 # The short X.Y version
 version = u'2.12'
 # The full version, including alpha/beta/rc tags
-release = u'2.12.9'
+release = u'2.12.10'
 
 
 # -- General configuration ---------------------------------------------------
