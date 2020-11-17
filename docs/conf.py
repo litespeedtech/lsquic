e02bc3157d0de31249300437fa86dd7da1a088f6
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2020-11-17 13:57:13 -0500

    Release 2.12.14
    - [BUGFIX] min RTO delay is 200 ms , not 1 second.
    - [BUGFIX] calculate TLP timer correctly when only one packet is in
      flight.
    - [BUGFIX] discard CRYPTO frames from lower levels after connection
      promotion.
    - [BUGFIX] cancel path response if path could not be initialized.
    - [BUGFIX] if push promise fails, do not invoke hset destructor.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'LiteSpeed Technologies'
 # The short X.Y version
 version = u'2.12'
 # The full version, including alpha/beta/rc tags
-release = u'2.12.11'
+release = u'2.12.14'
 
 
 # -- General configuration ---------------------------------------------------
