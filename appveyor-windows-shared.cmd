4f6af099e19941ee41016f71950e8facc83a896b
Tyler Young <tyler.young@stirlinglabs.com>
2022-01-10 11:32:57 -0500

    improve vcpkg integration
    use pcre2 over pcre on windows (avoids weird import linkage issue under shared build)
    
    try installing debs directly from cache instead of apt updating
    
    copy depended dlls as needed for tests, bins
    
    fix regex lookup xplat

--- appveyor-windows-shared.cmd
+++ appveyor-windows-shared.cmd
@@ -1,13 +1,23 @@
 setlocal EnableDelayedExpansion
+for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do call "%%~a\VC\Auxiliary\Build\vcvars64.bat"
+
+set VCPKG_ROOT=c:/tools/vcpkg/
+
+pushd c:\tools\vcpkg\
+dir /a /s /b *pcre*.h
+popd
+
 vcpkg list
 
-if exist ".\boringssl\CMakeLists.txt" (
+for /f %%t in (boringssl-target.txt) do set BORINGSSL_TARGET=%%t
+
+if exist ".\boringssl\include\openssl\ssl.h" (
     echo boringssl cached
 ) else (
     git clone https://boringssl.googlesource.com/boringssl
     cd boringssl
-    for /f %%t in (boringssl-target.txt) do set BORINGSSL_TARGET=%%t
     git checkout %BORINGSSL_TARGET%
+    rd /s /q .git
     cmake -DCMAKE_GENERATOR_PLATFORM=x64 --config Debug -DBUILD_SHARED_LIBS=ON -DOPENSSL_NO_ASM=1 .
     msbuild /m crypto\crypto.vcxproj
     if errorlevel 1 exit !errorlevel!
@@ -26,8 +36,6 @@ set /a retry_submodule_update+=1
 git submodule update --checkout --force --recursive
 if %retry_submodule_update% gtr 10 exit !errorlevel!
 if errorlevel 1 goto :retry_submodule_update
-
-set VCPKG_ROOT=c:/tools/vcpkg/
 
 cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DLSQUIC_SHARED_LIB=ON -DBUILD_SHARED_LIBS=ON -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DBORINGSSL_DIR=%cd%\boringssl -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON .
 
--- appveyor-windows-test.cmd
+++ appveyor-windows-test.cmd
@@ -1,15 +1,18 @@
+for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do call "%%~a\VC\Auxiliary\Build\vcvars64.bat"
+
 msbuild /m RUN_TESTS.vcxproj
 if errorlevel 1 exit !errorlevel!
 
-msbuild /m perf_server.vcxproj
+wevtutil qe Application /q:"*[System[TimeCreated[timediff(@SystemTime) <= 1209600000]]]" /f:text /c:1
+
+msbuild /m bin\perf_server.vcxproj
 if errorlevel 1 exit !errorlevel!
-msbuild /m perf_client.vcxproj
+msbuild /m bin\perf_client.vcxproj
 if errorlevel 1 exit !errorlevel!
 
-.\bin\perf_server -L notice -s ::1:8443 -c localhost,tests/localhost.pem,tests/localhost.key
+start "" /B bin\Debug\perf_server -L notice -s ::1:8443 -c localhost,tests/localhost.pem,tests/localhost.key
 
-echo Started @ %time%
-.\bin\perf_client -L info -s ::1:8443 -p 104857600:104857600
-echo Finished @ %time%
+bin\Debug\perf_client -L info -s ::1:8443 -p 104857600:104857600
 
 tskill perf_server
+
