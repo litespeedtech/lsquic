836b0deadbfe9c7fa886a1cdfcc7e1db6675f934
Hadi Frohar <46677591+hadifrohar@users.noreply.github.com>
2023-06-23 19:20:30 +0300

    fix freeing frame before handling (#463)
    Co-authored-by: hadif <hadif@checkpoint.com>

--- src/liblsquic/lsquic_di_hash.c
+++ src/liblsquic/lsquic_di_hash.c
@@ -438,12 +438,10 @@ hash_di_insert_frame (struct data_in *data_in,
     ins = lsquic_data_in_hash_insert_data_frame(data_in, data_frame,
                                                                 read_offset);
     assert(ins != INS_FRAME_OVERLAP);
-    /* NOTE: Only release packet and frame for INS_FRAME_OK,
-     *        other cases are handled by caller */
+    /* NOTE: other cases are handled by caller */
     if (ins == INS_FRAME_OK)
     {
         lsquic_packet_in_put(hdi->hdi_conn_pub->mm, new_frame->packet_in);
-        lsquic_malo_put(new_frame);
     }
     return ins;
 }
