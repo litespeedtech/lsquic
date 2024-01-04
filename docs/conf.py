5ed1d4cab585f0d33f4eefac72f65f8dbf203a05
George Wang <gwang@litespeedtech.com>
2024-01-04 11:59:56 -0500

    explicitly set html_theme = "sphinx_rtd_theme"

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ primary_domain = 'c'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_style = '/default.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
