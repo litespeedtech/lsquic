0fa020998cfcb6d7311aa552faba9f14b6a4ae89
George Wang <gwang@litespeedtech.com>
2024-01-04 12:44:59 -0500

    remove html_sytle = /default.css .

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ primary_domain = 'c'
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_style = '/default.css'
+#html_style = '/default.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
