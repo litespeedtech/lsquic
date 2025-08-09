ffef696d6bf844812188ec0011b459cae1c391b6
Neil Horman <nhorman@openssl.org>
2025-08-09 10:49:13 -0400

    Update appveyor to use new LIBSSL_DIR macro

--- appveyor-windows.cmd
+++ appveyor-windows.cmd
@@ -17,7 +17,7 @@ git submodule update --checkout --force --recursive
 
 set VCPKG_ROOT=c:/tools/vcpkg/
 
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_SHARED_LIBS=OFF -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DGETOPT_INCLUDE_DIR=c:/tools/vcpkg/installed/x64-windows/include  -DGETOPT_LIB=c:/tools/vcpkg/installed/x64-windows/lib/getopt.lib -DBORINGSSL_DIR=%cd%\boringssl .
+cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_SHARED_LIBS=OFF -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DGETOPT_INCLUDE_DIR=c:/tools/vcpkg/installed/x64-windows/include  -DGETOPT_LIB=c:/tools/vcpkg/installed/x64-windows/lib/getopt.lib -DLIBSSL_DIR=%cd%\boringssl .
 
 
 msbuild /m ALL_BUILD.vcxproj
