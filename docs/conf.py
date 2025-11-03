8bd478e60c81ae841046fbc801774293a9ba73fe
Dmitri Tikhonov <dtikhonov@live.com>
2025-11-03 18:45:40 -0500

    Release 4.3.2 (#577)

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'LiteSpeed Technologies'
 # The short X.Y version
 version = u'4.3'
 # The full version, including alpha/beta/rc tags
-release = u'4.3.1'
+release = u'4.3.2'
 
 
 # -- General configuration ---------------------------------------------------
