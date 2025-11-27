6b4bdb7961c406c6841ac7483ca1311e70f92bdc
Dmitri Tikhonov <dtikhonov@live.com>
2025-11-27 07:59:51 -0500

    Revert this silly file

--- src/liblsquic/lsquic-config.cmake
+++ src/liblsquic/lsquic-config.cmake
@@ -1,18 +1,4 @@
+@PACKAGE_INIT@
 
-####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
-####### Any changes to this file will be overwritten by the next CMake run ####
-####### The input file was lsquic-config.cmake                            ########
-
-get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)
-
-macro(set_and_check _var _file)
-  set(${_var} "${_file}")
-  if(NOT EXISTS "${_file}")
-    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
-  endif()
-endmacro()
-
-####################################################################################
-
-set_and_check(lsquic_INCLUDE_DIR "${PACKAGE_PREFIX_DIR}/include")
+set_and_check(lsquic_INCLUDE_DIR "@PACKAGE_CMAKE_INSTALL_INCLUDEDIR@")
 include("${CMAKE_CURRENT_LIST_DIR}/lsquic-targets.cmake")
