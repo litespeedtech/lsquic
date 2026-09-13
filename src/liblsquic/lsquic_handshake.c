2b301897969b9cab1d7df86098108f4381f37df7
Dmitri Tikhonov <dtikhonov@live.com>
2026-09-13 09:38:41 -0400

    Revert "Fix: gQUIC: handle memory allocation failure when appending SNI (#685)"
    This reverts commit 226092101112695080173d6639ab11184cd3ad7f.

--- src/liblsquic/lsquic_handshake.c
+++ src/liblsquic/lsquic_handshake.c
@@ -868,11 +868,8 @@ lsquic_enc_session_create_client (struct lsquic_conn *lconn, const char *domain,
     enc_session->enpub = enpub;
     enc_session->cid   = cid;
     enc_session->info  = info;
-    if (0 != lsquic_str_append(&enc_session->hs_ctx.sni, domain, strlen(domain)))
-    {
-        lsquic_enc_session_destroy(enc_session);
-        return NULL;
-    }
+    /* FIXME: allocation may fail */
+    lsquic_str_append(&enc_session->hs_ctx.sni, domain, strlen(domain));
     maybe_log_secrets(enc_session);
     if (lconn->cn_version >= LSQVER_050)
     {
